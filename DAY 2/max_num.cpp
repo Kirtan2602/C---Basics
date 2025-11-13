eb05372fae76cf115dd834748f50c231cab9629d
Kirtan Parmar <pkirtan2602@gmail.com>
2025-11-13 22:58:37 +0530

    Added Day 2 programs (arrays, loops, etc.)

---
+++ DAY 2/max_num.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter number of element: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+         max = arr[i];
+    }
+
+    cout << "Maximum element = " << max << endl;
+    return 0;
+    
+    
+}
